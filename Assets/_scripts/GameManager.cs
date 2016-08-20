22892241c7af3f95ef05277a76b55a3ddaed821a
arnaud gorain <arnaud gorain>
2016-08-19 22:55:51 -0400

    Add player index pre-logic

---
+++ Assets/_scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class GameManager : MonoBehaviour {
+
+    private PlayerIndex player_index;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    PlayerIndex GetPlayerIndex()
+    {
+        return player_index;
+    }
+
+    void SetPlayerIndex(PlayerIndex p_PlayerIndex)
+    {
+        player_index = p_PlayerIndex;
+    }
+}
