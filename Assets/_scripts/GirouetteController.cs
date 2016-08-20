a69b69bc378edd3dcf9210ea7b539b02f461d827
arnaud gorain <arnaud gorain>
2016-08-20 03:20:12 -0400

    Main join script done, still some bugs

---
+++ Assets/_scripts/GirouetteController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class GirouetteController : MonoBehaviour {
+
+    public PlayerIndex player_index;
+
+    // Use this for initialization
+    void Start () {
+        player_index = (PlayerIndex) 4;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.LogFormat(player_index.ToString());
+        GamePadState currentState = GamePad.GetState(player_index);
+    }
+}
--- Assets/_scripts/JoinScript.cs
+++ Assets/_scripts/JoinScript.cs
@@ -5,38 +5,104 @@ using XInputDotNetPure;
 
 public class JoinScript : MonoBehaviour {
 
-    public GameManager gameManager;
-    private PlayerIndex player_index;
+    PlayerIndex player_index;
+    int nNbPlayerActivated;
 
-    public GameObject playerPrefab;
-    public GameObject playerInstance;
+    GameObject playerPrefab;
+    GameObject playerInstance0;
+    GameObject playerInstance1;
+    GameObject playerInstance2;
+    GameObject playerInstance3;
+
+    bool bJoinPress;
+    int[] nJoinedArray;
 
     // Use this for initialization
-   /* void Start () {
-        player_index = gameManager.GetPlayerIndex();
+    void Start () {
+        nNbPlayerActivated = 0;
+        bJoinPress = false;
+
+        nJoinedArray = new int[4];
+        for (int j = 0; j < 4; j++)
+        {
+            nJoinedArray[j] = 4;
+        }
     }
 	
 	// Update is called once per frame
-	void Update () {
-        GamePadState currentState = GamePad.GetState(player_index);
-
-        if (playerInstance == null)
+	void Update ()
+    {
+        Debug.LogFormat("OOOOOOH");
+        if ((playerInstance0 == null && nNbPlayerActivated == 0) || (playerInstance1 == null && nNbPlayerActivated == 1) || (playerInstance2 == null && nNbPlayerActivated == 2) || (playerInstance3 == null && nNbPlayerActivated == 3))
         {
-            //check to see if the player pushed the A button
-            if (currentState.Buttons.A == ButtonState.Pressed)
+            for (int i = 0; i < 4; i++)
             {
-                playerInstance = (GameObject)Instantiate(playerPrefab, transform.position, transform.rotation);
-                playerInstance.GetComponent<CharacterControllerPotatoe>().playerIndex = player_index;
-                StartCoroutine(Vibrate(0.25f));
+                Debug.LogFormat("YEAAAAH");
+                Debug.LogFormat(((PlayerIndex)i).ToString());
+                GamePadState currentState = GamePad.GetState((PlayerIndex) i);
+
+                bool bSkip = false;
+
+                // skip, to not have multiple time the same player
+                for (int j = 0; j < 4; j++)
+                {
+                     if (nJoinedArray[j] == i)
+                        bSkip = true;
+                }
+
+                if (!bSkip)
+                {
+                    //check to see if the player pushed the A button
+                    if (currentState.Buttons.A == ButtonState.Pressed)
+                    {
+                        bJoinPress = true;
+                    }
+
+                    if (currentState.Buttons.A == ButtonState.Released && bJoinPress)
+                    {
+                        bJoinPress = false;
+
+                        Debug.LogFormat("SET ITTTTT***********************");
+                        Debug.LogFormat(((PlayerIndex)i).ToString());
+
+                        // Set player index
+                        switch (i)
+                        {
+                            case 0:
+                                Debug.LogFormat("FIRST");
+                                playerInstance0 = GameObject.Find("Girouette");
+                                playerInstance0.GetComponent<GirouetteController>().player_index = (PlayerIndex) i;
+                                break;
+                            case 1:
+                                Debug.LogFormat("DOS");
+                                playerInstance1 = GameObject.Find("Character (1)");
+                                playerInstance1.GetComponent<CharController>().player_index = (PlayerIndex) i;
+                                break;
+                            case 2:
+                                playerInstance2 = GameObject.Find("Character (2)");
+                                playerInstance2.GetComponent<CharController>().player_index = (PlayerIndex) i;
+                                break;
+                            case 3:
+                                playerInstance3 = GameObject.Find("Character (3)");
+                                playerInstance3.GetComponent<CharController>().player_index = (PlayerIndex) i;
+                                break;
+                            default:
+                                break;
+                        }
+
+                        StartCoroutine(Vibrate(0.25f, (PlayerIndex) i - 1));
+                        nNbPlayerActivated++;
+                    }
+                }
             }
         }
         else
         {
             //NOTE:  Doesn't work with some XInput emulated device drivers like the popular PS3 Controller one
             //destroy the player instance if the controller disconnects
-            if (currentState.IsConnected == false)
+           /* if (currentState.IsConnected == false)
             {
-                Destroy(playerInstance);
+                Destroy(playerInstance0);
                 return;
             }
             else
@@ -45,34 +111,17 @@ public class JoinScript : MonoBehaviour {
                 //destroy the player instance if the player pushed the Back button
                 if (currentState.Buttons.Back == ButtonState.Pressed)
                 {
-                    Destroy(playerInstance);
+                    Destroy(playerInstance0);
                     return;
                 }
-
-                if (currentState.PacketNumber > lastPacketNumber)
-                {
-                    lastPacketNumber = currentState.PacketNumber;
-                    lastPacketTime = Time.time;
-                }
-                else
-                {
-                    //NOTE:  Doesn't work with some XInput emulated device drivers like the popular PS3 Controller one
-                    if (Time.time - lastPacketTime > 20)
-                    {
-                        //controller has been idle for 20 seconds
-                        StartCoroutine(Vibrate(0.5f));
-                        Destroy(playerInstance);
-                    }
-                }
-
-            }
+            }*/
         }
     }
 
-    IEnumerator Vibrate(float duration)
+    IEnumerator Vibrate(float duration, PlayerIndex p_player)
     {
-        GamePad.SetVibration(player_index, 1.0f, 0.0f);
+        GamePad.SetVibration(p_player, 1.0f, 0.0f);
         yield return new WaitForSeconds(duration);
-        GamePad.SetVibration(player_index, 0.0f, 0.0f);
-    }*/
+        GamePad.SetVibration(p_player, 0.0f, 0.0f);
+    }
 }
