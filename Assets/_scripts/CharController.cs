e6c8e24a9642036b2fba47b3a3417c17f1a43936
Catalin <catalinp86@gmail.com>
2016-08-19 23:37:46 -0400

    char script plus spotlight off at start

--- Assets/_scripts/CharController.cs
+++ Assets/_scripts/CharController.cs
@@ -5,21 +5,40 @@ using XInputDotNetPure;
 public class CharController : MonoBehaviour {
 
     public PlayerIndex player_index;
+    public GameObject char_light;
+
+    private bool char_activated = false;
+    private Rigidbody body;
+    private Light char_spotlight;
 
     // Use this for initialization
     void Start () {
-	    
-	}
+        body = GetComponent<Rigidbody>();
+        body.isKinematic = true;
+
+        char_spotlight = char_light.GetComponent<Light>();
+        char_spotlight.enabled = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		GamePadState currentState = GamePad.GetState(player_index);
-		Debug.Log (currentState.Buttons.A.ToString());
-        /*currentState = GamePad.GetState(PlayerIndex.Two);
-        Debug.Log(currentState);
-        currentState = GamePad.GetState(PlayerIndex.Three);
-        Debug.Log(currentState);
-        currentState = GamePad.GetState(PlayerIndex.Four);
-        Debug.Log(currentState);*/
+        GamePadState currentState = GamePad.GetState(player_index);
+
+        if (char_activated)
+        {
+            // inti dsgsdfgsfasfasdf
+        }
+        else
+        {
+            if (currentState.Buttons.A == ButtonState.Pressed)
+            {
+                char_activated = true;
+                body.isKinematic = false;
+                char_spotlight.enabled = true;
+            }
+        }
+		
+		//Debug.Log (.ToString());
+
     }
 }
