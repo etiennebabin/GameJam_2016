c34b119db7cc508524a9e947c04762d934bfd87c
arnaud gorain <arnaud gorain>
2016-08-20 07:33:32 -0400

    Join script, simple but working

--- Assets/_scripts/GirouetteController.cs
+++ Assets/_scripts/GirouetteController.cs
@@ -8,12 +8,10 @@ public class GirouetteController : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        player_index = (PlayerIndex) 4;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //Debug.LogFormat(player_index.ToString());
         GamePadState currentState = GamePad.GetState(player_index);
     }
 }
--- Assets/_scripts/JoinScript.cs
+++ Assets/_scripts/JoinScript.cs
@@ -8,7 +8,6 @@ public class JoinScript : MonoBehaviour {
     PlayerIndex player_index;
     int nNbPlayerActivated;
 
-    GameObject playerPrefab;
     GameObject playerInstance0;
     GameObject playerInstance1;
     GameObject playerInstance2;
@@ -32,75 +31,86 @@ public class JoinScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        Debug.LogFormat("OOOOOOH");
-        if ((playerInstance0 == null && nNbPlayerActivated == 0) || (playerInstance1 == null && nNbPlayerActivated == 1) || (playerInstance2 == null && nNbPlayerActivated == 2) || (playerInstance3 == null && nNbPlayerActivated == 3))
+        /*for (int i = 0; i < 4; i++)
         {
-            for (int i = 0; i < 4; i++)
+            GamePadState currentState = GamePad.GetState((PlayerIndex)i);
+
+            //check to see if the player pushed the A button
+            if (currentState.Buttons.A == ButtonState.Pressed)
+            {
+                bJoinPress = true;
+            }
+
+            if (currentState.Buttons.A == ButtonState.Released && bJoinPress)
+                StartCoroutine(Vibrate(0.25f, (PlayerIndex)i));
+        }*/
+
+        /////////////////////////////
+
+        /*Debug.LogFormat("OOOOOOH");
+        for (int i = 0; i < 4; i++)
+        {
+            if (nJoinedArray[i] == 4)
             {
                 Debug.LogFormat("YEAAAAH");
                 Debug.LogFormat(((PlayerIndex)i).ToString());
-                GamePadState currentState = GamePad.GetState((PlayerIndex) i);
+                GamePadState currentState = GamePad.GetState((PlayerIndex)i);
 
-                bool bSkip = false;
-
-                // skip, to not have multiple time the same player
-                for (int j = 0; j < 4; j++)
+                //check to see if the player pushed the A button
+                if (currentState.Buttons.A == ButtonState.Pressed)
                 {
-                     if (nJoinedArray[j] == i)
-                        bSkip = true;
+                    bJoinPress = true;
                 }
 
-                if (!bSkip)
+                if (currentState.Buttons.A == ButtonState.Released && bJoinPress)
                 {
-                    //check to see if the player pushed the A button
-                    if (currentState.Buttons.A == ButtonState.Pressed)
+                    bJoinPress = false;
+
+                    Debug.LogFormat("SET ITTTTT***********************");
+                    Debug.LogFormat(((PlayerIndex)i).ToString());
+                    Debug.LogFormat(nNbPlayerActivated.ToString());
+
+                    nJoinedArray[i] = i;
+
+                    // Set player index
+                    if (playerInstance0 == null)
                     {
-                        bJoinPress = true;
+                        Debug.LogFormat("FIRST");
+                        playerInstance0 = GameObject.Find("Girouette");
+                        playerInstance0.GetComponent<GirouetteController>().player_index = (PlayerIndex)i;
+                    }
+                    else if (playerInstance1 == null)
+                    {
+                        Debug.LogFormat("DOS");
+                        playerInstance1 = GameObject.Find("Character (1)");
+                        playerInstance1.GetComponent<CharController>().player_index = (PlayerIndex)i;
+                    }
+                    else if (playerInstance2 == null)
+                    {
+                        Debug.LogFormat("TRES");
+                        playerInstance2 = GameObject.Find("Character (2)");
+                        playerInstance2.GetComponent<CharController>().player_index = (PlayerIndex)i;
+                    }
+                    else if (playerInstance3 == null)
+                    {
+                        Debug.LogFormat("QUATRO");
+                        playerInstance3 = GameObject.Find("Character (3)");
+                        playerInstance3.GetComponent<CharController>().player_index = (PlayerIndex)i;
                     }
 
-                    if (currentState.Buttons.A == ButtonState.Released && bJoinPress)
-                    {
-                        bJoinPress = false;
-
-                        Debug.LogFormat("SET ITTTTT***********************");
-                        Debug.LogFormat(((PlayerIndex)i).ToString());
-
-                        // Set player index
-                        switch (i)
-                        {
-                            case 0:
-                                Debug.LogFormat("FIRST");
-                                playerInstance0 = GameObject.Find("Girouette");
-                                playerInstance0.GetComponent<GirouetteController>().player_index = (PlayerIndex) i;
-                                break;
-                            case 1:
-                                Debug.LogFormat("DOS");
-                                playerInstance1 = GameObject.Find("Character (1)");
-                                playerInstance1.GetComponent<CharController>().player_index = (PlayerIndex) i;
-                                break;
-                            case 2:
-                                playerInstance2 = GameObject.Find("Character (2)");
-                                playerInstance2.GetComponent<CharController>().player_index = (PlayerIndex) i;
-                                break;
-                            case 3:
-                                playerInstance3 = GameObject.Find("Character (3)");
-                                playerInstance3.GetComponent<CharController>().player_index = (PlayerIndex) i;
-                                break;
-                            default:
-                                break;
-                        }
-
-                        StartCoroutine(Vibrate(0.25f, (PlayerIndex) i - 1));
-                        nNbPlayerActivated++;
-                    }
+                    StartCoroutine(Vibrate(0.25f, (PlayerIndex) i));
                 }
+
+                Debug.LogFormat("IF CHECK DONE");
             }
+
+            Debug.LogFormat("BOUCLE DONE");
         }
-        else
+        /*else
         {
             //NOTE:  Doesn't work with some XInput emulated device drivers like the popular PS3 Controller one
             //destroy the player instance if the controller disconnects
-           /* if (currentState.IsConnected == false)
+            if (currentState.IsConnected == false)
             {
                 Destroy(playerInstance0);
                 return;
@@ -114,8 +124,8 @@ public class JoinScript : MonoBehaviour {
                     Destroy(playerInstance0);
                     return;
                 }
-            }*/
-        }
+            }
+        }*/
     }
 
     IEnumerator Vibrate(float duration, PlayerIndex p_player)
